501397bd1475f92b45b62e5344accee29eeaeda5
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-09-19 10:38:59 -0400

    finish

--- nested.py
+++ nested.py
@@ -3,13 +3,53 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "Your Github Username"
+__author__ = "andrewpchristensen701"
 
 import sys
 
-def main(args):
-    """Add your code here"""
-    
+openp = ['(', '[', '{', '<', '(*']
+closep = [')', ']', '}', '>', '*)']
+
+
+def test_string(entered):
+    count = 0
+    stack = []
+    unbalanced = False
+    while entered:
+        item = entered[0]
+
+        if entered.startswith("(*"):
+            item = "(*"
+        elif entered.startswith("*)"):
+            item = "*)"
+        count += 1
+
+        if item in closep:
+            index = closep.index(item)
+            matching = open[index]
+
+            if stack.pop() != matching:
+                unbalanced = True
+                break
+
+        if item in open:
+            stack.append(item)
+
+        entered = entered[len(item):]
+
+    if stack or unbalanced:
+        return 'NO ' + str(count)
+    return 'YES'
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        with open('output.txt', 'w') as o:
+            for entered in f:
+                read_output = test_string(entered)
+                print(read_output)
+                o.write(read_output + '\n')
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
